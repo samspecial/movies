9aca2fd7a853bbc7b8fee832f9d3fbe91179eaf4
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-10-02 03:25:19 +0100

    modified movies

--- js/app.js
+++ js/app.js
@@ -22,6 +22,30 @@ function watchNow(e, selector) {
   window.location.href = selector;
 }
 
-let visaCard = /^ 4[0 - 9]{ 12}(?: [0 - 9]{ 3})?$/
+// Form Validation
+let button = document.getElementById("button");
+let creditCardPin = document.getElementById("creditCard");
+let form = document.getElementById("form");
+button.addEventListener("click", submitForm);
 
-masterCard = /^ 5[1 - 5][0 - 9]{ 14 } $/
+function submitForm(e) {
+  e.preventDefault();
+  let pinValue = creditCardPin.value;
+
+
+  console.log(pinValue)
+  const visaCard = RegExp(/^4[0-9]{12}(?:[0-9]{3})?$/);
+  const masterCard = RegExp(/^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/);
+
+  if (visaCard.test(pinValue) || masterCard.test(pinValue)) {
+
+    alert("Login Successful.");
+    form.reset();
+    return newPinValue.join();
+  } else {
+    alert("Please provide a valid Visa or Mastercard")
+  }
+
+}
+
+
